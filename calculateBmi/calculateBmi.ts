f5ed5dcad74f2d1382b217dfa14c2f1e80e6ce8d
laptop <juha.jylhakoski@aalto.fi>
2021-08-28 13:42:35 +0800

    9.1

---
+++ calculateBmi/calculateBmi.ts
@@ -0,0 +1,34 @@
+
+// $ npm init 
+
+// $ npm install -g ts-node typescript
+
+// $ npm install --save-dev @types/node
+
+// $ npm run ts-node -- calculateBmi.ts
+
+// $ npm run calculateBmi
+
+ const calculateBmi = (a: number, b: number) : string => {
+
+ 	const normal = 13500
+
+ 	const normal_message = "Normal (healthy weight)"
+
+	console.log(a * b)
+
+	if ((a * b) < normal) {
+		return normal_message
+	}
+}
+
+const a: number = Number(process.argv[2])
+	
+const b: number = Number(process.argv[3])
+
+// 9.1
+console.log(calculateBmi(180, 74))
+
+// $ npm run calculateBmi 180 74
+
+console.log(calculateBmi(a, b))
